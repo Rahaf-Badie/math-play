b4ae8f6f0324f055a6292297a650de7deb374e0a
Rahaf-Badie <princessmoon393@gmail.com>
2025-10-26 16:38:06 +0200

    commit6

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('mathplay.home', absolute: false));
+        return redirect()->intended(route('mathplay.home'));
     }
 
     /**
@@ -45,6 +45,6 @@ class AuthenticatedSessionController extends Controller
     $request->session()->regenerateToken();
 
     // إعادة التوجيه للصفحة الرئيسية العامة بعد تسجيل الخروج
-    return Redirect::route('mathplay.index');
+    return redirect()->route('mathplay.home');
 }
 }
